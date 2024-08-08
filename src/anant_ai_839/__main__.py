c344272e1fe3b452d581850b7d307f8909051b8b
Anant Mohan <anant4830@gmail.com>
2024-08-08 21:58:40 +0530

    first commit

---
+++ src/anant_ai_839/__main__.py
@@ -0,0 +1,18 @@
+"""Anant-ai-839 file for ensuring the package is executable
+as `anant-ai-839` and `python -m anant_ai_839`
+"""
+from pathlib import Path
+
+from kedro.framework.cli.utils import find_run_command
+from kedro.framework.project import configure_project
+
+
+def main(*args, **kwargs):
+    package_name = Path(__file__).parent.name
+    configure_project(package_name)
+    run = find_run_command(package_name)
+    run(*args, **kwargs)
+
+
+if __name__ == "__main__":
+    main()
